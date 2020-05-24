6d8ec48f9f88bdd39349857e114ef001085a2968
LeMikado <micscarito@gmail.com>
2020-05-24 20:11:30 +0200

    up

--- Raspberry Pi - Python/RobotProject/test_interface.py
+++ Raspberry Pi - Python/RobotProject/test_interface.py
@@ -4,11 +4,11 @@ import time
 import os
 
 def selecting_song(avail_songs,cor_seq): # main
-    #s("To select a song, a melody will be played.")
-    #s("You can navigate between all available songs with the first and third tile.")
-    #s("If you are okay with the song, press the second tile.")
-    #s("Once selected, you will be able to play.")
-    #s("Some L E Ds will show you where to press.")
+    s("To select a song, a melody will be played.")
+    s("You can navigate between all available songs with the first and third tile.")
+    s("If you are okay with the song, press the second tile.")
+    s("Once selected, you will be able to play.")
+    s("Some L E Ds will show you where to press.")
 
     time.sleep(0.5)
 
